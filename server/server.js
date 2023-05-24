2a83b9d097c74314d68fc892a41085da8be617b1
Tony Rizzotto <tony@tonyrizzotto.com>
2023-05-23 21:23:35 -0400

    feat: refactor logging to align with Cloud Run

--- server/server.js
+++ server/server.js
@@ -5,14 +5,43 @@ import FastifyVite from '@fastify/vite';
 import { renderToString } from 'react-dom/server';
 import { v4 } from 'uuid';
 import api from './api/index.js';
-import loggingConfig from './utils/index.js';
+import loggingConfig, { shouldNotLogOutput } from './utils/index.js';
 
 export default async function createServer({ environment }) {
   const server = Fastify({
-    logger: loggingConfig[environment] || true,
+    disableRequestLogging: true,
+    ignoreTrailingSlash: true, // supports both trailing and none
+    logger: loggingConfig[environment],
     requestIdHeader: 'x-request-id',
     requestIdLogLabel: 'requestId',
     genReqId: () => v4(),
+  });
+  const onRequestLogging = async (request) => {
+    // In production, lets not log any request to the assets directory
+    if (shouldNotLogOutput(request.url)) {
+      return;
+    }
+    request.log.info({
+      message: `Request received: ${request.method} ${request.url}`,
+      headers: request.headers,
+      method: request.method,
+      url: request.url,
+    });
+  };
+
+  server.addHook('onRequest', onRequestLogging);
+
+  server.addHook('onResponse', async (req, res) => {
+    if (shouldNotLogOutput(req.url)) {
+      return;
+    }
+    req.log.info({
+      message: `Request completed: ${req.method} ${req.url} ${Math.round(res.getResponseTime())}ms`,
+      headers: req.headers,
+      method: req.method,
+      url: req.url,
+      response: res,
+    });
   });
 
   // Register entire api
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,21 +1,58 @@
+const defaultOpts = {
+  messageKey: 'message',
+  level: 'info',
+  levelKey: 'severity',
+  formatters: {
+    level(label) {
+      return { severity: label };
+    },
+  },
+  serializers: {
+    response: (reply) => ({
+      statusCode: reply.statusCode,
+      responseTime: reply.getResponseTime(),
+    }),
+  },
+};
+
 const loggingConfig = {
   local: {
+    ...defaultOpts,
     transport: {
       target: 'pino-pretty',
       options: {
+        levelKey: 'severity',
+        messageKey: 'message',
         translateTime: 'HH:MM:ss Z',
         ignore: 'pid,hostname',
         colorize: true,
       },
     },
-    formatters: {
-      level(label) {
-        return { severity: label };
-      },
-    },
   },
-  production: true,
+  production: { ...defaultOpts },
   test: false,
 };
 
 export default loggingConfig;
+
+/*
+This is useful in local logging to prevent the logging of static resources in SSR mode
+*/
+export function shouldNotLogOutput(url) {
+  if (url.startsWith('/assets')
+  // apply any paths in local that you don't want to see in your logs
+  || (url.startsWith('/Pages'))
+  || (url.startsWith('/contexts'))
+  || (url.startsWith('/queries'))
+  || (url.startsWith('/Routes'))
+  || (url.startsWith('/mount'))
+  || (url.startsWith('/createApp'))
+  || (url.startsWith('/App'))
+  || (url.startsWith('/@fs'))
+  || (url.startsWith('/@vite'))
+  || (url.startsWith('/@react'))
+  ) {
+    return true;
+  }
+  return false;
+}
